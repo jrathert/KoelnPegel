307a5e02ea86b6e9b067357a1772f0c10718ceb3
Jonas Rathert <jrt@gmx.de>
2023-01-17 22:48:26 +0100

    Add tests for environment variables

---
+++ environment_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestReadEnvironment(t *testing.T) {
+	readEnvironment("kpg.env")
+	lst := []string{"SERVER", "CLIENT_ID", "CLIENT_SECRET", "ACCESS_TOKEN"}
+	for _, key := range lst {
+		if _, ok := os.LookupEnv(key); !ok {
+			t.Errorf(`readEnvironment: env key %v not present`, key)
+		}
+	}
+	// if os.Getenv("SERVER") != "https://social.cologne" {
+	// }
+
+}
